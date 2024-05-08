1753f1952f4fbcb124d0b1798a112b4c6cf4e451
KeepFocusL <1281438594@qq.com>
2024-05-08 20:46:37 +0800

    练习

---
+++ lxh-se/src/day240508/practice/Practice1.java
@@ -0,0 +1,11 @@
+package day240508.practice;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <=100 ; i++) {
+            sum +=i;
+        }
+        System.out.println(sum);
+    }
+}
