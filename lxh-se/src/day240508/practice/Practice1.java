cc164a93af6960062bd98132af09093bab2b784d
KeepFocusL <1281438594@qq.com>
2024-05-08 20:55:39 +0800

    练习 - 直接用jdk自带的currentTimeMillis求出差值

--- lxh-se/src/day240508/practice/Practice1.java
+++ lxh-se/src/day240508/practice/Practice1.java
@@ -3,10 +3,17 @@ package day240508.practice;
 public class Practice1 {
     public static void main(String[] args) {
         //需求:如何计算出这个算法的运行时间
+        //思路:
+        //记录开始时间
+        long start = System.currentTimeMillis();
         int sum = 0;
         for (int i = 1; i <=100 ; i++) {
             sum +=i;
         }
         System.out.println(sum);
+        //记录结束时间
+        long end = System.currentTimeMillis();
+        //计算开始时间到结束时间的差值
+        System.out.println(start - end);
     }
 }
