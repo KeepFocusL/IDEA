f319a850ce395aed503e03d897beeeb47a79d60d
KeepFocusL <1281438594@qq.com>
2024-05-08 20:26:52 +0800

    abstract step - 1 增加abstract方法(类也必须是抽象类)

--- lxh-se/src/day240508/abstract_demo/oop/Animal.java
+++ lxh-se/src/day240508/abstract_demo/oop/Animal.java
@@ -1,8 +1,10 @@
 package day240508.abstract_demo.oop;
 
-public class Animal {
+public abstract class Animal {
     String name;
     public Animal(String name) {
         this.name = name;
     }
+
+    public abstract void info();
 }
