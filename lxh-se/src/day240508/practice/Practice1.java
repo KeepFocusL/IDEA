929998ed51b0718b1ba83cebc1c7c1471870ed8e
KeepFocusL <1281438594@qq.com>
2024-05-08 20:51:01 +0800

    练习 - 新需求：如何计算出这个算法的运行时间

--- lxh-se/src/day240508/practice/Practice1.java
+++ lxh-se/src/day240508/practice/Practice1.java
@@ -2,6 +2,7 @@ package day240508.practice;
 
 public class Practice1 {
     public static void main(String[] args) {
+        //需求:如何计算出这个算法的运行时间
         int sum = 0;
         for (int i = 1; i <=100 ; i++) {
             sum +=i;
