f1c4fce9691d143e1f49ac5f60e438a6e1e819a5
KeepFocusL <1281438594@qq.com>
2024-05-08 20:24:36 +0800

    删除了两个不需要的子类

--- lxh-se/src/day240508/abstract_demo/oop/Test.java
+++ lxh-se/src/day240508/abstract_demo/oop/Test.java
@@ -7,9 +7,6 @@ public class Test {
         person.adopt(cat);
         Dog dog = new Dog("小白");
         person.adopt(dog);
-        Pig pig = new Pig("猪哥");
-        person.adopt(pig);
-        Rabbit rabbit = new Rabbit("笨蛋");
-        person.adopt(rabbit);
+
     }
 }
