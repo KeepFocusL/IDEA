6e257b0f32e815d08335cda4598a7147abc98b15
KeepFocusL <1281438594@qq.com>
2024-05-12 15:12:42 +0800

    MySortUtil - 让 print 方法支持打印所有 MyObject[] 类型 (add)

--- lxh-se/src/day240512/p/MySortUtil.java
+++ lxh-se/src/day240512/p/MySortUtil.java
@@ -1,6 +1,19 @@
 package day240512.p;
 
 public class MySortUtil {
+    public static void print(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void print(MyObjectUtil[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
 
     public static void sort(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
@@ -18,11 +31,20 @@ public class MySortUtil {
             }
         }
     }
-    public static void print(MyObjectUtil[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+    public static void sort(MyObjectUtil[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < (arr.length - 1) - i; j++) {
+                if (arr[j].xxx > arr[j + 1].xxx) {
+                    MyObjectUtil temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
         }
-        System.out.println();
     }
 }
-
