c0c5e3a169fd8cd3a3d581e96b60a60ec65898a2
KeepFocusL <1281438594@qq.com>
2024-05-12 15:07:37 +0800

    MySortUtil - 支持排序/打印 TV[] (修复上一个 commit 遗留下来的问题)...

--- lxh-se/src/day240512/p/TV.java
+++ lxh-se/src/day240512/p/TV.java
@@ -1,11 +1,12 @@
 package day240512.p;
 
-public class TV extends MyObjectUtil{
+public class TV extends MyObjectUtil {
     //TV[]（按价格 price）
     double price;
 
     public TV(double price) {
         this.price = price;
+        this.xxx = this.price;
     }
 
     @Override
