17664689b58ce9c4743041bfb7b53152a1cdc4f6
KeepFocusL <1281438594@qq.com>
2024-05-12 10:09:46 +0800

    MySortUtil - 为什么验证是否排序成功 - 增加支持打印 int[]

--- lxh-se/src/day240512/p/MySortUtil.java
+++ lxh-se/src/day240512/p/MySortUtil.java
@@ -1,15 +1,22 @@
 package day240512.p;
 
 public class MySortUtil {
+    public static void print(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
     public static void sort(int[] arr) {
         // 写具体的排序算法，可以回顾之前写过的 BubbleSort
         for (int i = 0; i < arr.length - 1; i++) {
             boolean swapped = false;
             for (int j = 0; j < (arr.length - 1) - i; j++) {
-                if (arr[j] > arr[j+1]) {
+                if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                     swapped = true;
                 }
             }
--- lxh-se/src/day240512/p/Test.java
+++ lxh-se/src/day240512/p/Test.java
@@ -8,5 +8,6 @@ public class Test {
         // 需求：支持排序 int[]
         int[] intArr = new int[] {5, 3, 9, 2, 1};
         MySortUtil.sort(intArr);
+        MySortUtil.print(intArr);
     }
 }
