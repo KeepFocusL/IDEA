e32959a9707884593a484c88678b6062d9fc1cf1
KeepFocusL <1281438594@qq.com>
2024-05-12 10:08:19 +0800

    MySortUtil - 轮廓（以终为始）

---
+++ lxh-se/src/day240512/p/MySortUtil.java
@@ -0,0 +1,8 @@
+package day240512.p;
+
+public class MySortUtil {
+    public static void sort(int[] arr) {
+        // 写具体的排序算法，可以回顾之前写过的 BubbleSort
+    }
+}
+
---
+++ lxh-se/src/day240512/p/Test.java
@@ -0,0 +1,12 @@
+package day240512.p;
+
+/**
+ * 以终为始
+ */
+public class Test {
+    public static void main(String[] args) {
+        // 需求：支持排序 int[]
+        int[] intArr = new int[] {5, 3, 9, 2, 1};
+        MySortUtil.sort(intArr);
+    }
+}
