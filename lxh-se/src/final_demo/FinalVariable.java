74ee2170b5c15eaa31a08b822c66b2839d1e0d99
KeepFocusL <1281438594@qq.com>
2024-05-16 16:58:38 +0800

    final - 修饰变量 - Instance Variables (Non-Static Fields) 必须赋值 - 在`代码块`中赋初始值

--- lxh-se/src/final_demo/FinalVariable.java
+++ lxh-se/src/final_demo/FinalVariable.java
@@ -3,14 +3,17 @@ package final_demo;
 public class FinalVariable {
     final int instanceVariable;
 
-    public FinalVariable() {
+    /*public FinalVariable() {
         // 如果注释下面的代码，就类似于留了一个后门，
         // 从这个后门进来时 instanceVariable 没有初始值，与 final 冲突了
         instanceVariable = 1;
+    }*/
 
-    }
-
-    public FinalVariable(int instanceVariable) {
+    /*public FinalVariable(int instanceVariable) {
         this.instanceVariable = instanceVariable;
+    }*/
+
+    {
+        instanceVariable = 10;
     }
 }
