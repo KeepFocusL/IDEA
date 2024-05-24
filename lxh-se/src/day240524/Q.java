db6a78afe72767b955e09c4acf74e3aff97ef471
KeepFocusL <1281438594@qq.com>
2024-05-24 22:45:05 +0800

    面试题 Integer赋值打印的结果 (看源码找答案)

---
+++ lxh-se/src/day240524/Q.java
@@ -0,0 +1,28 @@
+package day240524;
+
+public class Q {
+    public static void main(String[] args) {
+        q1();
+        q2();
+        q3();
+    }
+
+    static void q1() {
+        Integer i1 = new Integer(1);
+        Integer i2 = new Integer(1);
+        System.out.println(i1 == i2);
+    }
+
+    static void q2() {
+        Integer i1 = 1;
+        Integer i2 = 1;
+        System.out.println(i1 == i2);
+    }
+
+    static void q3() {
+        Integer i1 = 222;
+        Integer i2 = 222;
+        System.out.println(i1 == i2);
+    }
+
+}
