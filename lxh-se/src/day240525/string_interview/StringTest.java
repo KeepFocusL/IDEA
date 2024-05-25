dd8b18cfabb7812f492cddd4bd7c56c387b296a8
KeepFocusL <1281438594@qq.com>
2024-05-25 10:56:27 +0800

    String 面试题 - Q1 ==

---
+++ lxh-se/src/day240525/string_interview/StringTest.java
@@ -0,0 +1,12 @@
+package day240525.string_interview;
+
+public class StringTest {
+    @SuppressWarnings({"all"})
+    public static void main(String[] args) {
+        String sq = new String("xyz");
+        String sz = new String("xyz");
+        System.out.println(sq == sz);
+        
+
+    }
+}
