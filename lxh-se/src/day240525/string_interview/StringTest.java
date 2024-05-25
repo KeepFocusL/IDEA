8500b855500860b36e27119b4739aab9bc4948d0
KeepFocusL <1281438594@qq.com>
2024-05-25 12:03:44 +0800

    String 面试题 - Q3/Q5 final s1 变成常量 ---> 常量池

--- lxh-se/src/day240525/string_interview/StringTest.java
+++ lxh-se/src/day240525/string_interview/StringTest.java
@@ -16,7 +16,7 @@ public class StringTest {
 
         System.out.println("=== === ===");
 
-        String s1 = "Java";
+        final String s1 = "Java";
         String s2 = "2312";
 
         String s3 = "Java2312";
@@ -27,12 +27,14 @@ public class StringTest {
         String s7 = s1 + s2;
 
         System.out.println(s3 == s4);//字符串字面量 + 字符串字面量 ---> 优化 ---> 常量池
-        System.out.println(s3 == s5);
+        System.out.println(s3 == s5);//final s1 常量 ---> 常量池
         System.out.println(s3 == s6);
         System.out.println(s3 == s7);
-        System.out.println(s4 == s5);
+        System.out.println(s4 == s5);//final s1 常量 ---> 常量池
         System.out.println(s5 == s6);
         System.out.println(s6 == s7);
+
+        System.out.println("=== === ===");
 
         String s8 = s6.intern();//.intern() 要求主动走常量池，而不是走s6中的s2地址
         System.out.println(s3 == s8);
