72e49bf08717e8c2adb1106256379c8d556214bf
KeepFocusL <1281438594@qq.com>
2024-05-27 23:11:15 +0800

    阅读源码时间 - 从阅读源码中获取灵感，自定义 MyStringBuilder，模拟核心功能 (error)

---
+++ lxh-se/src/day240525/string_demo/MyStringBuilder.java
@@ -0,0 +1,5 @@
+package day240525.string_demo;
+
+public class MyStringBuilder {
+    char[] value;
+}
--- lxh-se/src/day240525/string_demo/Test3.java
+++ lxh-se/src/day240525/string_demo/Test3.java
@@ -7,5 +7,11 @@ public class Test3 {
         //    背景：对于数组来说，需要确定类型和长度，同时长度一旦确定，就不能改变
         //    作答方式：阅读 StringBuilder 源码，截取阅读源码过程中你觉得是能说这个问题的关键代码配图，加上文字说明
         //    前提：断网，只能阅读源代码
+
+        // 从阅读源码中获取灵感，然后：自定义一个 MyStringBuilder，模拟 StringBuilder 的核心功能。
+        // 以终为始的方式
+        MyStringBuilder myStringBuilder = new MyStringBuilder();
+        myStringBuilder.append("abc").append("xyz");// 期待的功能：类似 StringBuilder，可以一直 append(...)
+        System.out.println(myStringBuilder);//期待的输出：abcxyz
     }
 }
