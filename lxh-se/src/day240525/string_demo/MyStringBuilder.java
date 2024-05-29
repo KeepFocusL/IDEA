21db0fecd76b22b5fb0936cda09341afae3d195c
KeepFocusL <1281438594@qq.com>
2024-05-29 13:18:06 +0800

    阅读源码时间 - MyStringBuilder 重写 toString 方法（IDEA自动生成的方式）

--- lxh-se/src/day240525/string_demo/MyStringBuilder.java
+++ lxh-se/src/day240525/string_demo/MyStringBuilder.java
@@ -1,9 +1,18 @@
 package day240525.string_demo;
 
+import java.util.Arrays;
+
 public class MyStringBuilder {
     char[] value;
 
     public MyStringBuilder append(String str) {
         return this;
+    }
+
+    @Override
+    public String toString() {
+        return "MyStringBuilder{" +
+                "value=" + Arrays.toString(value) +
+                '}';
     }
 }
