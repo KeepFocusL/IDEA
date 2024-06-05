f3983f301cfb60c6a51f97ca39406c949a4b75f7
KeepFocusL <1281438594@qq.com>
2024-06-05 18:54:06 +0800

    阅读源码 - 获取到的死循环写法

---
+++ lxh-se/src/day240531/LearnFromReadingSourceCode.java
@@ -0,0 +1,15 @@
+package day240531;
+
+public class LearnFromReadingSourceCode {
+    public static void main(String[] args) {
+        // 之前知道的死循环写法
+        /*while (true) {
+            System.out.println("====");
+        }*/
+
+        // 今天从阅读 AbstractCollection 的 toString 源码知道的死循环写法
+        /*for (;;) {
+            System.out.println("=== === ===");
+        }*/
+    }
+}
