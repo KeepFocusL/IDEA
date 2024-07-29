f665ecb9944d7f014a9ff8c199cb618145968a93
KeepFocusL <1281438594@qq.com>
2024-07-29 11:20:15 +0800

    App - 具体的轮廓

---
+++ lxh-se/src/day240729/App.java
@@ -0,0 +1,19 @@
+package day240729;
+
+public class App {
+    public static void main(String[] args) {
+
+        //读取配置文件
+
+        //=== === ===
+        //以boss的视角，定义规范
+        //定义具体的落地(黑盒子的思想):抽象类/接口
+        //Downloader
+
+        //Parser
+
+        //Repository
+
+        //Notificator
+    }
+}
