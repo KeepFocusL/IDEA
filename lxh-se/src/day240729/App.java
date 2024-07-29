f8ab3aa09dc2f7942fb42d1a239f34d2d9db4c64
KeepFocusL <1281438594@qq.com>
2024-07-29 12:46:38 +0800

    App - 定义Downloader规范  (error) - 以终为始

--- lxh-se/src/day240729/App.java
+++ lxh-se/src/day240729/App.java
@@ -1,18 +1,28 @@
 package day240729;
 
+import day240729.parser.Parser;
+
+import javax.jnlp.DownloadService;
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
 
         //读取配置文件
+        System.out.println("文件配置设置完成");
 
         //=== === ===
         //以boss的视角，定义规范
         //定义具体的落地(黑盒子的思想):抽象类/接口
         //Downloader
+        Downloader downloader = new Downloader();
 
         //Parser
 
         //Repository
+
+
 
         //Notificator
     }
