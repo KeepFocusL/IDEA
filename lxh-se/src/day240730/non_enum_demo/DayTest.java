81280a9f72af30bbe7d27b3a33274a2c2f8eb70e
KeepFocusL <1281438594@qq.com>
2024-07-30 15:53:44 +0800

    模仿enum的特性,写出可以直接sout出值，类型是本身的类

---
+++ lxh-se/src/day240730/non_enum_demo/DayTest.java
@@ -0,0 +1,9 @@
+package day240730.non_enum_demo;
+
+public class DayTest {
+    public static void main(String[] args) {
+        System.out.println(Day.MONDAY);
+        Day friday = Day.FRIDAY;
+        System.out.println(friday);
+    }
+}
