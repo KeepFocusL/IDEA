cdf265b64be30a80f08c4b8083f85b5e439001ad
KeepFocusL <1281438594@qq.com>
2024-07-30 15:52:56 +0800

    enum 类型是Day本身,sout可以直接打印出值

---
+++ lxh-se/src/day240730/enum_demo/Day.java
@@ -0,0 +1,12 @@
+package day240730.enum_demo;
+
+public enum Day {
+    MONDAY,
+    TUESDAY,
+    MEDNESDAY,
+    THURDAY,
+    FIRDAY,
+    SATURDAY,
+    SUNDAY;
+
+}
---
+++ lxh-se/src/day240730/enum_demo/DayTest.java
@@ -0,0 +1,8 @@
+package day240730.enum_demo;
+
+public class DayTest {
+    public static void main(String[] args) {
+        Day monday = Day.MONDAY;
+        System.out.println(Day.MONDAY);
+    }
+}
