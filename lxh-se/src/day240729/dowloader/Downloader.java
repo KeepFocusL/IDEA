9a87d1fd692bd105714529f231ee03cc1fad0d19
KeepFocusL <1281438594@qq.com>
2024-07-30 11:54:58 +0800

    App(Downloader) - 完善 getDownloader 逻辑，用 Reflection 反射的方式代替 if...else 判断

--- lxh-se/src/day240729/dowloader/Downloader.java
+++ lxh-se/src/day240729/dowloader/Downloader.java
@@ -1,18 +1,18 @@
 package day240729.dowloader;
 
+import java.lang.reflect.Constructor;
+
 public abstract class Downloader {
     public static Downloader getDownloader(String downloaderName) {
-        // 1. 从配置文件获取获取 downloader 配置的值
-        // 2. if ... else
-        Downloader downloader;
-        if ("nope".equals(downloaderName)) {
-            downloader = new NopeDownloader();
-        } else if ("jsoup".equals(downloaderName)){
-            downloader = new JsoupDownloader();
-        } else {
-            downloader = null;
+        Downloader dl;
+        try {
+            Class<?> downloaderClass = Class.forName(downloaderName);
+            Constructor<?> declaredConstructor = downloaderClass.getDeclaredConstructor();
+            dl = (Downloader) declaredConstructor.newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-        return downloader;
+        return dl;
     }
 
     public abstract String download(String url);
