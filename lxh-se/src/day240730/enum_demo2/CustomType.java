bfbfa55251e2501af351eacf2a6420b7a5b69492
KeepFocusL <1281438594@qq.com>
2024-07-31 14:42:52 +0800

    enum - 内容还是可以定义 state 和 behavior，但是要注意编写的位置

--- lxh-se/src/day240730/enum_demo2/CustomType.java
+++ lxh-se/src/day240730/enum_demo2/CustomType.java
@@ -2,5 +2,11 @@ package day240730.enum_demo2;
 
 public enum CustomType {
     JAVA,
-    PYTHON
+    PYTHON;//如果这里之后不用写东西可以省略;
+
+    String desc;
+
+    public String getDesc() {
+        return desc;
+    }
 }
