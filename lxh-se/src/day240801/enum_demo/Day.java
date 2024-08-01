05ad045d5c615b89dedc99b11dd28994e559599f
KeepFocusL <1281438594@qq.com>
2024-08-01 19:55:51 +0800

    enum - 可以在枚举内的具体每个对象内部实现接口定义的方法...
    本质上在内部自动new出来了,所以每个值都是对象,继承了抽象接口所以都要实现出来

--- lxh-se/src/day240801/enum_demo/Day.java
+++ lxh-se/src/day240801/enum_demo/Day.java
@@ -1,17 +1,46 @@
 package day240801.enum_demo;
 
 public enum Day implements FofEnumDemo {
-    MONDAY,
-    TUESDAY,
-    MEDNESDAY,
-    THURDAY,
-    FIRDAY,
-    SATURDAY,
-    SUNDAY;
+    MONDAY {
+        @Override
+        public void desc() {
+            System.out.println("XXX");
+        }
+    },
+    TUESDAY {
+        @Override
+        public void desc() {
 
+        }
+    },
+    MEDNESDAY {
+        @Override
+        public void desc() {
 
-    @Override
-    public void desc() {
-        System.out.println("XXX");
+        }
+    },
+    THURDAY {
+        @Override
+        public void desc() {
+
+        }
+    },
+    FIRDAY {
+        @Override
+        public void desc() {
+
+        }
+    },
+    SATURDAY {
+        @Override
+        public void desc() {
+
+        }
+    },
+    SUNDAY {
+        @Override
+        public void desc() {
+
+        }
     }
 }
