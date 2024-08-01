a881a763ea44fbe2d5e1d578d788b30e017dc4e7
KeepFocusL <1281438594@qq.com>
2024-08-01 20:12:56 +0800

    Thread 初见 - 单例 + 多线程 - 直观查看线程不安全的情况

--- lxh-se/src/day240801/thread_base/StudentTest.java
+++ lxh-se/src/day240801/thread_base/StudentTest.java
@@ -2,8 +2,17 @@ package day240801.thread_base;
 
 public class StudentTest {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
+        /*for (int i = 0; i < 5; i++) {
             System.out.println(Student.getInstance());
+        }*/
+
+        for (int i = 0; i < 5; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(Student.getInstance());
+                }
+            }).start();  //.start()开启多条线程
         }
     }
 }
