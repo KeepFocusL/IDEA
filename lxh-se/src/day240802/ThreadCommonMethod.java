8c9b03cda5189f98124b989f111eb625ba5df457
KeepFocusL <1281438594@qq.com>
2024-08-04 22:45:16 +0800

    Thread - 常用方法 - Thread.yield

--- lxh-se/src/day240802/ThreadCommonMethod.java
+++ lxh-se/src/day240802/ThreadCommonMethod.java
@@ -3,7 +3,8 @@ package day240802;
 public class ThreadCommonMethod {
     public static void main(String[] args) throws InterruptedException {
         //sleep();
-        join();
+        //join();
+        testYield();
     }
 
 
@@ -31,4 +32,20 @@ public class ThreadCommonMethod {
             System.out.println(Thread.currentThread().getName() + "---" + i);
         }
     }
+
+    private static void testYield() {
+        new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                System.out.println(Thread.currentThread().getName() + "---" + i);
+                if (i % 10 == 0) {
+                    Thread.yield();
+                }
+            }
+        }).start();
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(Thread.currentThread().getName() + "---" + i);
+        }
+    }
 }
+
