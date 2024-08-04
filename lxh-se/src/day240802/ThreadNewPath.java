9b44bddd8e8d7ab647a06b8d80347a7c715404a6
KeepFocusL <1281438594@qq.com>
2024-08-04 20:20:11 +0800

    Thread - 启动一条新的执行路径的两个方法...
    1.extends Thread
    2.implements Runnable

---
+++ lxh-se/src/day240802/ThreadNewPath.java
@@ -0,0 +1,36 @@
+package day240802;
+
+/**
+ * 如何启动一个条新的执行路径？
+ * <p>
+ * 方式一：extends Thread && Override run() && start()
+ * 方式二：implements Runnable && implement run() && new Thread(...).start()
+ * 方法其他：还有其他方式，本例子暂时先介绍这两种，后续再补充
+ */
+public class ThreadNewPath {
+    public static void main(String[] args) {
+        Thread myThread1 = new MyThread1();
+        myThread1.start();
+
+        Thread myThread2 = new Thread(new MyThread2());
+        myThread2.start();
+    }
+}
+
+class MyThread1 extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(this.getName() + "---" + i);
+        }
+    }
+}
+
+class MyThread2 implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(Thread.currentThread().getName() + "---" + i);
+        }
+    }
+}
