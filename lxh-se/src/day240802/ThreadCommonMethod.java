cf4b620253c797d44a5ecff30f51b5cbfa1eab3c
KeepFocusL <1281438594@qq.com>
2024-08-04 22:23:41 +0800

    Thread - 常用方法 - join...
    在当前线程中加入指定线程，使得这个指定线程等待当前线程，并在当前线程结束前结束。

--- lxh-se/src/day240802/ThreadCommonMethod.java
+++ lxh-se/src/day240802/ThreadCommonMethod.java
@@ -2,12 +2,33 @@ package day240802;
 
 public class ThreadCommonMethod {
     public static void main(String[] args) throws InterruptedException {
-        sleep();
+        //sleep();
+        join();
     }
+
 
     private static void sleep() throws InterruptedException {
         System.out.println("1");
         Thread.sleep(1000);
         System.out.println("2");
+    }
+
+    private static void join() {
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                System.out.println(Thread.currentThread().getName() + "---" + i);
+            }
+        });
+        t1.start();
+
+        try {
+            t1.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName() + "---" + i);
+        }
     }
 }
