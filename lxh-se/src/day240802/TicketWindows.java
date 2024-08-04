85015505cd23b385bfb86036b79d9b0f902c5035
KeepFocusL <1281438594@qq.com>
2024-08-04 23:17:37 +0800

    Thread - TicketWindows 模拟车站买票的场景 - ArrayList

---
+++ lxh-se/src/day240802/TicketWindows.java
@@ -0,0 +1,34 @@
+package day240802;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 模拟车站买票的场景：总共有 10000 张票，同时开放 10 个窗口对外售卖
+ * <p>
+ * 写一个程序，模拟该场景
+ * <p>
+ * 分析下面的程序：会不会出问题？会出什么问题？为什么？如何改进？
+ */
+public class TicketWindows {
+    static List<String> tickets = new ArrayList<>();
+
+    static {
+        for (int i = 0; i < 10000; i++) {
+            tickets.add("车票编码---" + i);
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (!tickets.isEmpty()) {
+                        System.out.println("售出===" + tickets.remove(0));
+                    }
+                }
+            }).start();
+        }
+    }
+}
